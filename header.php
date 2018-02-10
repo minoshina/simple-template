cae5e5621bb0a1b3c4a9ee653acbc1f7840a2682
minoshina <nakamura24862@gmail.com>
2018-02-10 11:44:51 +0900

    Initial commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+content="width=device-width, initial-scale=1.0 ">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+  <div class="header-inner">
+
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div class="container">
+  <div class="contents">
+      <p>TEST</p>
+
+  </div>
+  <?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
